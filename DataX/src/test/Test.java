2a86a9b18b1acffe3134779e20a3bc367be6fe14
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-04-07 14:45:40 -0700

    u

--- DataX/src/test/Test.java
+++ DataX/src/test/Test.java
@@ -3,10 +3,8 @@ package test;
 import coin.DataX.data.Database;
 import coin.DataX.data.Schema;
 
-import java.io.IOException;
-
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Database database = new Database("src/test/database");
         Schema schema = new Schema(database, "data");
     }
